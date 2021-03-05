dc98f0a0ab98f1a33dcc32b5d240a7ce44a44b2a
Aidan Potteiger <Gamesitfo@gmail.com>
2021-03-04 18:15:18 -0600

    Ready for deployment

--- SCP/SCP.py
+++ SCP/SCP.py
@@ -2,12 +2,17 @@ import discord
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
+import subprocess
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = discord.Client()
 client = commands.Bot(command_prefix=">")
+
+triggered = False
+
+scpServer_Path = 'C:\\Program Files (x86)\\Steam\\steamapps\\common\\SCP Secret Laboratory Dedicated Server'
 
 # EVENT LISTENER FOR WHEN THE BOT HAS SWITCHED FROM OFFLINE TO ONLINE.
 @bot.event
@@ -28,22 +33,27 @@ async def on_ready():
 
 # EVENT LISTENER FOR WHEN A NEW MESSAGE IS SENT TO A CHANNEL.
 @bot.event
-async def on_message(self, message):
-	# CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
-	if message.content == "hello":
-		# SENDS BACK A MESSAGE TO THE CHANNEL.
-		await message.channel.send("hi!")
-	if self.user.mentioned_in(message) and message.mention_everyone is False:
-		if 'help' in message.content.lower():
-			await message.channel.send('Eine volle Liste aller Commands gibts hier: https://github.com/Der-Eddy/discord_bot#commands-list')
-		else:
-			await message.add_reaction('👀') # :eyes:
-@client.event    
-async def on_message(self, message):
-    for x in message.mentions:
-        if(x==client.user):
-            await message.channel.send(f":sauropod: did someone mention me?")
+async def on_message(ctx):
+	global message
+	if ctx.content == "hello":
+		await ctx.channel.send("hi!")
+	if discord.utils.get(ctx.guild.members) in ctx.mentions:
+		message = await ctx.channel.send("Hey! Do you want to start the SCP Server? (React with 7 or more to start)")
+		print(type(message))
+		await message.add_reaction('🥵')
 
-    await client.process_commands(message)
-# EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
+
+@bot.event
+async def on_reaction_add(reaction, user):
+	global triggered
+	for re in reaction.message.reactions:
+		if re.emoji != '🥵':
+			print('Reaction rejected')
+			await message.clear_reaction(re)
+	if reaction.count >= 2 and triggered == False:
+		await reaction.message.channel.send('Starting up!')
+		triggered = True
+
+def startServer():
+	subprocess.call(scpServer_Path)
 bot.run(TOKEN)
